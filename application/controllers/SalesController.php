d54bb9177ec1ad93d78e79a700d5a408b0884afa
Bhavesh Rohera <roherabony@gmail.com>
2017-10-27 01:00:08 +0530

    Formatted

--- application/controllers/SalesController.php
+++ application/controllers/SalesController.php
@@ -1,41 +1,46 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class SalesController extends CI_Controller {
+class SalesController extends CI_Controller
+{
 
-	function __construct() {
-		parent::__construct();
-		if (!access()) {
-			logout();
-		} else {
-			$this->load->model('SalesModel');
-		}
-	}
+    function __construct()
+    {
+        parent::__construct();
+        if (!access()) {
+            logout();
+        } else {
+            $this->load->model('SalesModel');
+        }
+    }
 
-	public function index() {
-		$this->load->view('sales');
-	}
+    public function index()
+    {
+        $this->load->view('sales');
+    }
 
-	public function add() {
-		$data['page_title'] = 'Sales Add';
-		$currentBill = $this->SalesModel->getCurrentBillNo();
+    public function add()
+    {
+        $data['page_title'] = 'Sales Add';
+        $currentBill = $this->SalesModel->getCurrentBillNo();
         $data['currentBill'] = $currentBill;
-		$this->load->view('sales/sales_add', $data);
-	}
+        $this->load->view('sales/sales_add', $data);
+    }
 
-	public function getItemInfo()
-	{
-		$this->load->model('MyModel');
-		$this->MyModel->getItemInfo();
-	}
+    public function getItemInfo()
+    {
+        $this->load->model('MyModel');
+        $this->MyModel->getItemInfo();
+    }
 
-	public function salesBill()
+    public function salesBill()
     {
         $data['page_title'] = 'Sales Bill';
         $this->load->view('sales/sales_bill', $data);
     }
 
-    public function create() {
-	    $this->SalesModel->salesAdd();
+    public function create()
+    {
+        $this->SalesModel->salesAdd();
     }
 }
--- application/views/include/scripts.php
+++ application/views/include/scripts.php
@@ -8,7 +8,7 @@
 <script src="<?php echo base_url('assets/theme/bower_components/jquery-ui/jquery-ui.min.js'); ?>"></script>
 <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
 <script>
-  $.widget.bridge('uibutton', $.ui.button);
+    $.widget.bridge('uibutton', $.ui.button);
 </script>
 <!-- Bootstrap 3.3.7 -->
 <script src="<?php echo base_url('assets/theme/bower_components/bootstrap/dist/js/bootstrap.min.js'); ?>"></script>
