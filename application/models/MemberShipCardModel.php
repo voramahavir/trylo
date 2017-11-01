6fe4113294d042a19b6a07ee32fa6a257af66ac7
ajajpatel24 <pajaj93@gmail.com>
2017-11-01 09:19:02 +0530

    - Sorting in card listing is done

--- application/models/MemberShipCardModel.php
+++ application/models/MemberShipCardModel.php
@@ -31,6 +31,19 @@ class MemberShipCardModel extends CI_Model
         if (isset($_POST['draw'])) {
             $draw = $_POST['draw'];
         }
+        if(isset($_POST['order']) && count($_POST['order'])){
+            $column = $_POST['order'][0]['column'];
+            $sorttype = $_POST['order'][0]['dir'];
+            if($column == 0){
+                $this->db->order_by("CARDNO", $sorttype);
+            }elseif ($column == 1) {
+                $this->db->order_by("CARDNO1", $sorttype);
+            }elseif ($column == 2) {
+                $this->db->order_by("NAME", $sorttype);
+            }elseif ($column == 7) {
+                $this->db->order_by("CREADT", $sorttype);
+            }
+        }
         $output = array("code" => 0, "response" => array(),
             'draw' => $draw,
             'recordsTotal' => 0,
